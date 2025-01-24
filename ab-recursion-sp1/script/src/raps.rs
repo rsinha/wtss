084ea7bc6b898cac72c89d168d28d6022ac58376
Rohit Sinha <rohit@hashgraph.com>
2025-01-24 17:59:38 -0500

    adding simulation test

--- ab-recursion-sp1/script/src/raps.rs
+++ ab-recursion-sp1/script/src/raps.rs
@@ -150,3 +150,132 @@ fn generate_statement(
 
     (ab_curr_hash, ab_next_hash, statement)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use smallvec::ToSmallVec;
+    use sp1_sdk::include_elf;
+
+    /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
+    pub const AB_ROTATION_ELF: &[u8] = include_elf!("ab-rotation-program");
+
+    #[test]
+    fn run_simulation() {
+        // Setup the logger.
+        sp1_sdk::utils::setup_logger();
+
+        // Setup the program.
+        let (pk, vk) = RAPS::proof_setup(AB_ROTATION_ELF);
+
+        // AB 0 (genesis AB)
+        let (genesis_signing_keys, genesis_verifying_keys) = generate_signers::<5>();
+        let ab_genesis = AddressBook::new::<5>(
+            core::array::from_fn(|i| genesis_verifying_keys[i].to_bytes()),
+            [1; 5],
+        );
+
+        let genesis_signatures = subset_sign(
+            &genesis_signing_keys,
+            &[true; 5],
+            &RAPS::rotation_message(&ab_genesis, [0u8; 32]),
+        );
+
+        let ab_genesis_hash =
+            ab_rotation_lib::address_book::serialize_and_digest_sha256(&ab_genesis);
+        let genesis_proof = RAPS::construct_rotation_proof(
+            &pk,
+            &vk,
+            &ab_genesis_hash,
+            &ab_genesis,
+            &ab_genesis,
+            None,
+            &[0u8; 32],
+            &Signatures(genesis_signatures.to_smallvec()),
+        );
+
+        let mut prev_ab = ab_genesis;
+        let mut prev_proof = genesis_proof;
+        let mut prev_signing_keys = genesis_signing_keys;
+        let mut prev_verifying_keys = genesis_verifying_keys;
+
+        // simulate 10 rotations
+        for day in 0..15 {
+            assert!(RAPS::verify_proof(&vk, &prev_proof));
+            debug(&prev_proof);
+
+            let (next_signing_keys, next_verifying_keys) = if day % 2 == 0 {
+                // half of the days, we dont rotate
+                generate_signers::<5>()
+            } else {
+                (prev_signing_keys.clone(), prev_verifying_keys.clone())
+            };
+
+            let next_ab = AddressBook::new::<5>(
+                core::array::from_fn(|i| next_verifying_keys[i].to_bytes()),
+                [1; 5],
+            );
+
+            let signatures = subset_sign(
+                &prev_signing_keys,
+                &[true; 5],
+                &RAPS::rotation_message(&next_ab, [0u8; 32]),
+            );
+
+            let next_proof = RAPS::construct_rotation_proof(
+                &pk,
+                &vk,
+                &ab_genesis_hash,
+                &prev_ab,
+                &next_ab,
+                Some(prev_proof),
+                &[0u8; 32],
+                &Signatures(signatures.to_smallvec()),
+            );
+
+            prev_proof = next_proof;
+            prev_ab = next_ab;
+            prev_signing_keys = next_signing_keys;
+            prev_verifying_keys = next_verifying_keys;
+        }
+    }
+
+    fn generate_signers<const N: usize>() -> ([SigningKey; N], [VerifyingKey; N]) {
+        let keys: [(SigningKey, VerifyingKey); N] = std::array::from_fn(|_| RAPS::keygen());
+        let signing_keys: [SigningKey; N] = keys.clone().map(|sk| sk.0);
+        let verifying_keys: [VerifyingKey; N] = keys.map(|sk| sk.1);
+
+        (signing_keys, verifying_keys)
+    }
+
+    fn subset_sign<const N: usize>(
+        signing_keys: &[SigningKey; N],
+        signers: &[bool; N],
+        message: &[u8],
+    ) -> [Option<Signature>; N] {
+        core::array::from_fn(|i| signers[i].then_some(signing_keys[i].sign(message)))
+    }
+
+    fn debug(proof: &SP1ProofWithPublicValues) {
+        let parsed_proof =
+            PublicValuesStruct::abi_decode(&proof.public_values.to_vec(), true).unwrap();
+        println!("------------ BEGIN Roster Attestation Proof ------------");
+        println!(
+            "ab_genesis_hash: 0x{}",
+            &hex::encode(parsed_proof.ab_genesis_hash)[..8]
+        );
+        println!(
+            "ab_curr_hash:    0x{}",
+            &hex::encode(parsed_proof.ab_curr_hash)[..8]
+        );
+        println!(
+            "ab_next_hash:    0x{}",
+            &hex::encode(parsed_proof.ab_next_hash)[..8]
+        );
+        println!(
+            "tss_vk:          0x{}",
+            &hex::encode(parsed_proof.tss_vk_hash)[..8]
+        );
+        println!("------------ END Roster Attestation Proof ------------");
+    }
+}
