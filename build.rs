577615880373a90a25605f52c58962c65a736a8c
Rohit Sinha <rohit@hashgraph.com>
2025-02-11 11:31:34 -0500

    chore: deps and build

--- build.rs
+++ build.rs
@@ -1,8 +1,16 @@
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::path::{PathBuf, Path};
 
 fn main() {
+    // Build the RAPS project
+    build_raps();
+
+    // Copy the ELF file to the OUT_DIR
+    copy_elf();
+}
+
+fn copy_elf() {
     // Define the source file (inside a folder, e.g., "binaries/my_binary")
     let source = "./raps/target/elf-compilation/riscv32im-succinct-zkvm-elf/release/ab-rotation-program";
 
@@ -12,7 +20,34 @@ fn main() {
 
     // Copy the binary file
     fs::copy(source, &destination).expect("Failed to copy binary");
+}
 
-    // Tell Cargo to rerun if the source file changes
-    println!("cargo:rerun-if-changed={}", source);
+
+fn build_raps() {
+    // Get the path to the nested directory relative to the root project
+    let nested_dir = PathBuf::from("raps");
+
+    // Get the current directory (where build.rs is located)
+    let current_dir = env::current_dir().expect("Failed to get current directory");
+
+    // Combine paths to get the full path to the nested directory
+    let full_nested_path = current_dir.join(&nested_dir);
+
+    // Print directory for debugging
+    println!("cargo:warning=Building in directory: {}", full_nested_path.display());
+
+    // Run cargo build in the nested directory
+    let status = std::process::Command::new("cargo")
+        .current_dir(&full_nested_path)
+        .arg("build")
+        .arg("--release")
+        .status()
+        .expect("Failed to execute cargo build");
+
+    if !status.success() {
+        panic!("Failed to build nested project");
+    }
+
+    // Tell cargo to rerun this build script if files in the nested directory change
+    println!("cargo:rerun-if-changed={}", nested_dir.display());
 }
