ec52905710fabc2ad95dcec8dccb6196b6c5e310
Rohit Sinha <rohit@hashgraph.com>
2025-04-22 09:01:57 -0400

    renaming

--- succint-wraps/raps/build.rs
+++ succint-wraps/raps/build.rs
@@ -1,5 +1,5 @@
 use sp1_helper::build_program_with_args;
 
 fn main() {
-    build_program_with_args("../zkvm_program", Default::default())
+    build_program_with_args("../raps_zkvm_program", Default::default())
 }
